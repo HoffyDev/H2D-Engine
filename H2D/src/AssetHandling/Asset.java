0671a3dcbccb4e36c177418f4cd65a39933f0e71
HP <HP@HP8440p.speedport.ip>
2024-06-14 14:40:51 +0200

    Update

--- H2D/src/AssetHandling/Asset.java
+++ H2D/src/AssetHandling/Asset.java
@@ -1,16 +1,17 @@
 package AssetHandling;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 
 public class Asset {
 
 	public static final BufferedImage defaultImage = ImageLoader.loadImage("res/gfx/default.png");
 	
 	public static void initLoadingScreen() {
-		
+		//HASHMAP?
 	}
 	
-	public static void initRequired() {
+	public static void init() {
 		
 	}
 }
